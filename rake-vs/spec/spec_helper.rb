b9febab661624aeb9602031c731251eb0729dc03
locatw <loca.gm@gmail.com>
2014-03-23 18:58:38 +0900

    テスト用ソリューションデータをファイルから読み込む用に変更

--- rake-vs/spec/spec_helper.rb
+++ rake-vs/spec/spec_helper.rb
@@ -1,1 +1,12 @@
+def read_test_data(filename)
+  filepath = File.expand_path("../data", __FILE__) + "\\" + filename
+  raise "file not exists [#{filename}]" if not File.exists?(filepath)
+
+  contents = ""
+  File.open(filepath) do |f|
+    f.read(nil, contents)
+  end
+
+  contents
+end
 
