0845d0c1c1226d32a242d8c55e6cc32339c43d86
locatw <loca.gm@gmail.com>
2014-03-24 22:09:15 +0900

    ひとまとめにしていたVsSolutionのユニットテストケースを分割

--- spec/vs_solution_spec.rb
+++ spec/vs_solution_spec.rb
@@ -27,11 +27,23 @@ module RakeMSBuild
         expect(@vss.projects.size).to eq(1)
       end
 
-      it 'has a project that has correct propaties' do
+      it 'has a project that has correct name' do
         project = @vss.projects[0]
         expect(project.name).to eq('Project1')
+      end
+
+      it 'has a project that has correct path' do
+        project = @vss.projects[0]
         expect(project.path).to eq('ProjectPath')
+      end
+
+      it 'has a project that has correct guid' do
+        project = @vss.projects[0]
         expect(project.guid).to eq('AAA')
+      end
+
+      it 'has a project that has correct type' do
+        project = @vss.projects[0]
         expect(project.type).to eq(:cpp_project)
       end
     end
