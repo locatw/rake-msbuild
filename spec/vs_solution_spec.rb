95f81ade20def61870d0304f4f3f6917762af606
locatw <loca.gm@gmail.com>
2014-03-24 22:22:44 +0900

    VsSolutionのユニットテストに2つのプロジェクトを持つソリューションの読み込みを確認するテストケースを追加

--- spec/vs_solution_spec.rb
+++ spec/vs_solution_spec.rb
@@ -47,5 +47,47 @@ module RakeMSBuild
         expect(project.type).to eq(:cpp_project)
       end
     end
+
+    context 'that loaded a solution file with two project' do
+      before do
+        @vss.stub(:parse) {
+          project1 = {}
+          project1[:name] = 'Project1'
+          project1[:path] = 'ProjectPath1'
+          project1[:guid] = 'AAA'
+          project1[:type] = :cpp_project
+
+          project2 = {}
+          project2[:name] = 'Project2'
+          project2[:path] = 'ProjectPath2'
+          project2[:guid] = 'BBB'
+          project2[:type] = :cpp_project
+
+          [project1, project2]
+        }
+        @vss.stub(:read_sln) {}
+        @vss.load('not_exist.sln')
+      end
+
+      it 'has two project' do
+        expect(@vss.projects.size).to eq(2)
+      end
+
+      it 'has a project that has correct properties at first' do
+        project = @vss.projects[0]
+        expect(project.name).to eq('Project1')
+        expect(project.path).to eq('ProjectPath1')
+        expect(project.guid).to eq('AAA')
+        expect(project.type).to eq(:cpp_project)
+      end
+
+      it 'has a project that has correct properties at second' do
+        project = @vss.projects[1]
+        expect(project.name).to eq('Project2')
+        expect(project.path).to eq('ProjectPath2')
+        expect(project.guid).to eq('BBB')
+        expect(project.type).to eq(:cpp_project)
+      end
+    end
   end
 end
